fcb787a658ca6405f4872a9e3d6c5896d34811fa
Dimas Rocha <dimas.dasp@gmail.com>
2025-07-20 16:28:09 -0300

    findByDepartment implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -11,7 +11,7 @@ public class Seller implements Serializable {
 	
 	private Integer id;
 	private String name;
-	private String email;
+	private String email; 
 	private Date birthDate;
 	private Double baseSalary;
 
